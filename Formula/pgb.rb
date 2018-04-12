a69c6eb9ab9b4cf084f3a6b0ced02d64e6461545
goya <brettrudd@gmail.com>
2018-04-12 12:10:45 -0700

    Update new sha

--- Formula/pgb.rb
+++ Formula/pgb.rb
@@ -2,7 +2,7 @@ class Pgb < Formula
   desc "CLI for Adobe PhoneGap Build"
   homepage "https://build.phonegap.com"
   url "https://github.com/phonegap-build/pgb-cli/releases/download/v1.0.0/pgb.macos.tar.gz"
-  sha256 "b2f7cec8149108443ba68426c8fc3f1a440fba38481483f5270ad775310db0a0"
+  sha256 "dd98362993b7df4dfb87e9e10f3470e7f076f385c62c5bd96546aa4fa67e6343"
   version "1.0.0"
   bottle :unneeded
 
