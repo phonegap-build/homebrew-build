ad444f613eb1d65a864a590288483e70d667e8a3
goya <brettrudd@gmail.com>
2018-09-13 12:46:51 -0700

    New version

--- Formula/pgb.rb
+++ Formula/pgb.rb
@@ -1,9 +1,9 @@
 class Pgb < Formula
   desc "CLI for Adobe PhoneGap Build"
   homepage "https://build.phonegap.com"
-  url "https://github.com/phonegap-build/pgb-cli/releases/download/v1.0.3/pgb.macos.tar.gz"
-  sha256 "04b84eac735b147554267fc97926dbef0dd78a5cd5c6049b14e8ccfdefcae53c"
-  version "1.0.3"
+  url "https://github.com/phonegap-build/pgb-cli/releases/download/v1.1.0/pgb.macos.tar.gz"
+  sha256 "abb8568d2ae4f7a813725ab1ddb85c1b210cf809583ddcaf61f2acc31ad757d2"
+  version "1.1.0"
   bottle :unneeded
 
   def install
